a17538bd8f091fd227b354f39c0e3d4012746f65
JerryOffiicial <jerrysonjerry1234@email.com>
2026-01-23 22:43:28 +0530

    getAllMovies and getMoviesById

--- Movie-02/server/routes/movieRoutes.js
+++ Movie-02/server/routes/movieRoutes.js
@@ -1,9 +1,15 @@
 import express from "express";
 import upload from "../middleware/multer.js";
-import { addMovie } from "../controllers/movieController.js";
+import {
+  addMovie,
+  getAllMovies,
+  getMovieById,
+} from "../controllers/movieController.js";
 
 const movieRouter = express.Router();
 
-movieRouter.post("/add", upload.single("image"), addMovie)
+movieRouter.post("/add", upload.single("image"), addMovie);
+movieRouter.get("/all", getAllMovies);
+movieRouter.get("/:movieId", getMovieById);
 
 export default movieRouter;
